14d917638b4b00b9e2c218070abe8b5e3e3501f9
panedrone <sq;dalmaker@gmail.com>
2024-07-09 00:56:56 +0300

    [+] swag

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,14 @@
 package main
 
+// @schemes	http
+// @produce	json
+// @version	0.0.1
+//
+// @title		Burgers API
+// @in			header
+// @BasePath	/api
+// @accept		json
+// @host		127.0.0.1:8080
 func main() {
 
 }
